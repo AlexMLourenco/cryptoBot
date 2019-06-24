1c3e4db4bda472fa0dbfc033b8ccb660ff79669e
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2019-06-24 05:12:23 +0100

    corrected bug in the high of the candles

--- src/models/candlestick/index.js
+++ src/models/candlestick/index.js
@@ -1,10 +1,10 @@
 class Candlestick {
-    constructor({ low, hight, close, open, price, interval, startTime = new Date(), volume }) {
+    constructor({ low, high, close, open, price, interval, startTime = new Date(), volume }) {
         this.startTime = startTime
         this.interval = interval
         this.open = open //|| price
         this.close = close //|| price
-        this.hight = hight //|| price
+        this.high = high //|| price
         this.low = low //|| price
         this.volume = volume //|| 0
         //this.state = close ? 'closed' : 'open'
