f58b2c61772bb830b49c19502d96fe2c7489e192
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2019-06-25 01:01:07 +0100

    new options added for the command line

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 const program = require('commander')
-const Backtest = require('./src/backtest')
-const Historic = require('./src/historic')
+const Backtester = require('./src/backtest')
+const Trader = require('./src/trader')
 const config = require('./configuration')
+const Ticker = require('./src/ticker')
 
-// Time consts
 const now = new Date()
 const yesterday = new Date(now.getTime() - (24 * 60 * 60 * 1e3))
 
-// Convert from ms to s
 function toDate(val) {
-    return new Date(val * 1e3)
+  return new Date(val * 1e3)
 }
 
-// Opctions
 program.version('1.0.0')
     .option('-i, --interval [interval]', 'Interval in seconds for candlestick', parseInt)
     .option('-p, --product [product]', 'Product identifier', 'BTC-EUR')    
     .option('-s, --start [start]', 'Start time in unix seconds', toDate, yesterday)
     .option('-e, --end [end]', 'End time in unix seconds', toDate, now)
+    .option('-t, --strategy [strategy]', 'Strategy Type')
+    .option('-r, --type [type]', 'Run type')
+    .option('-f, --funds [funds]', 'Amount of money to use', parseInt)
+    .option('-l, --live', 'Run live')
     .parse(process.argv)
 
 const main = async function() {
-    const { interval, product, start, end } = program
+    const { interval, product, start, end, strategy, live, type, funds } = program
 
-    const tester = new Backtest({ start, end, product, interval })
-    
-    console.log(tester)
-    
-    await tester.start()
-    //const service = new Historic({ start, end, product, interval })
+    if (type == 'trader') {
+        const trader = new Trader({
+        start, end, product, interval, strategyType: strategy, live, funds
+        })
 
-    //const data = await service.getData()
-    //console.log(data)
+        await trader.start()
+    } else {
+        const tester = new Backtester({
+        start, end, product, interval, strategyType: strategy
+        })
+
+        await tester.start()
+    }
 }
 
 main()
